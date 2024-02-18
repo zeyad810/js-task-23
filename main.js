00743eee03fa60409fa3d74786e231ea5042bbfd
Zeyad Ashraf <115799167+zeyad810@users.noreply.github.com>
2024-02-18 19:46:46 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function basicCalc(num1, num2, operator) {
+    switch (operator) {
+        case "+":
+            return num1 + num2;
+            break;
+        case "-":
+            return num1 - num2;
+            break;
+        case "*":
+            return num1 * num2;
+            break;
+        case "/":
+            return num1 / num2;
+            break;
+
+        default: return num1 + num2
+            break;
+    }
+}
+console.log(basicCalc(5, 3 ,"+"));
+console.log(basicCalc(7,8, "*"));
+console.log(basicCalc(70,14, "/"));
+console.log(basicCalc(70,10, "-"));
+console.log(basicCalc(9,6));
